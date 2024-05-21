b36cd381e954ced94ca4e563d83cbafe855d1480
piyush <133100406+piyxshh@users.noreply.github.com>
2024-05-21 20:50:46 +0000

    algorithm

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,8 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
-
+import {ApiError} from "../utils/ApiError.js"
+import {User} from "../models/user.models.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler( async (req, res) => {
    // get user details from frontend
@@ -38,11 +41,44 @@ const registerUser = asyncHandler( async (req, res) => {
     }
 
 //check for images, avatar
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is required")
+    }
 
+   //upload on cloudinary
+  const avatar = await uploadOnCloudinary(avatarLocalPath)
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+  
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is required")
+  }
 
+//create user in db
 
+   const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase
+   })
 
+   const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+   )
 
+   if (!createdUser){
+    throw new ApiError(500, "Something went wrong while registering the user")
+   }
+
+   //retur response
+   return res.status(201).json(
+    new ApiResponse(200, createdUser, "User registered succesfully")
+   )
 
 })
 
